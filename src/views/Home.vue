4bf5cd1f7f6c0d09bd5b390f6e9cad2e96ca32db
dy2001 <953343675@qq.com>
2019-10-31 22:47:15 +0800

    更新首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <el-card class="box-card">
-        
+         <el-link type="primary" :underline="false" href="https://github.com/dy2001/Blog.Core.WebApi" target="_blank">Blog.Core(Blog WebApi项目)-GitHub</el-link><br/>
+         <el-link type="primary" :underline="false" href="https://github.com/dy2001/blog_admin" target="_blank">Blog.Admin(Blog后台项目)-GitHub</el-link><br/>
+         <el-link type="primary" :underline="false" href="https://github.com/dy2001/blog_vue" target="_blank">Blog.Vue(Blog主页项目)-GitHub</el-link><br/>
     </el-card>
   </div>
 </template>
